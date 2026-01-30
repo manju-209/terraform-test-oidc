b5911065d188c3975de099652035e313bcd87f58
manju-209 <manjukrishna.m@codework.ai>
2026-01-30 16:52:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux 2 (ap-south-1 example)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "GitHubActionsTerraformEC2"
+  }
+}
 
